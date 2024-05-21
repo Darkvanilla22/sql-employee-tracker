5cbd796f4b5ff1ad8f377737c91717016195e3b0
Finian Corbett <finian97@gmail.com>
2024-05-20 21:07:37 -0400

    Added function to display main menu and handle user input

--- index.js
+++ index.js
@@ -20,3 +20,51 @@ connection.connect(err => {
   console.log('Connected to the database.');
   runMainMenu();
 });
+
+// Function to display the main menu and handle user input
+function runMainMenu() {
+    inquirer.prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add a Department',
+        'Add a Role',
+        'Add an Employee',
+        'Update an Employee Role',
+        'Exit'
+      ]
+    })
+    .then(answer => {
+        switch(answer.action) {
+          case 'View All Departments':
+            viewDepartments();
+            break;
+          case 'View All Roles':
+            viewRoles();
+            break;
+          case 'View All Employees':
+            viewEmployees();
+            break;
+          case 'Add a Department':
+            addDepartment();
+            break;
+          case 'Add a Role':
+            addRole();
+            break;
+          case 'Add an Employee':
+            addEmployee();
+            break;
+          case 'Update an Employee Role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            connection.end();
+            console.log('Goodbye!');
+            break;
+        }
+      });
+    }
