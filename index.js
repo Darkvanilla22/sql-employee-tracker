520056fec5ef53cef37de9958e1458ad73703a34
Finian Corbett <finian97@gmail.com>
2024-05-20 23:06:24 -0400

    Fixed issue with database not being selected when running app

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT || 3306,  // Default to 3306 if DB_PORT is not specified
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_NAME
 });
 
 connection.connect(err => {
