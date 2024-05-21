a2e4e92d2914a3fde882f2dd3697deee558f8664
Finian Corbett <finian97@gmail.com>
2024-05-20 22:44:02 -0400

    Added create/use database check in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,29 @@
+-- Purpose: This file contains the schema for the database tables. The database is called employee_tracker_db and contains three tables: department, role, and employee.
+CREATE DATABASE IF NOT EXISTS employee_tracker_db;
+USE employee_tracker_db;
+
 -- Schema for department table
-CREATE TABLE department (
-    id INT PRIMARY KEY,
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 -- Schema for role table
-CREATE TABLE role (
-    id INT PRIMARY KEY,
+CREATE TABLE roles (
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
 -- Schema for employee table
-CREATE TABLE employee (
-    id INT PRIMARY KEY,
+CREATE TABLE employees (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
 );
